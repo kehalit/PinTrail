251537b77f580a2e02dbd05446e3b82b1e269230
Kehalit <88938730+kehalit@users.noreply.github.com>
2025-05-08 20:46:56 +0200

    refactor the tables

--- backend/datamanager/data_models.py
+++ backend/datamanager/data_models.py
@@ -10,7 +10,6 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
 
-
     trips = db.relationship("Trip", backref="user", cascade="all, delete-orphan")
 
     def to_dict(self):
