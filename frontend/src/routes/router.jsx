89fb628aec3e2295e159a08fd0fae530d3591d65
Kehalit <88938730+kehalit@users.noreply.github.com>
2025-06-03 11:13:25 +0200

    private route implemented to protect sensetive data

--- frontend/src/routes/router.jsx
+++ frontend/src/routes/router.jsx
@@ -5,16 +5,25 @@ import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
 import Dashboard from "../pages/Dashboard";
 import NotFound from "../pages/NotFound";
+import PrivateRoute from "../components/PrivateRoute";
 
 const Router = () => {
   return (
     <Routes>
-        
+
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route
+        path="/dashboard"
+        element={
+          <PrivateRoute>
+            <Dashboard />
+          </PrivateRoute>
+        }
+      />
       <Route path="*" element={<NotFound />} />
+
     </Routes>
   );
 };
