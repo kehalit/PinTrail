a2882387a35f0743c0456dec21b4a2c9e071b51a
Kehalit <88938730+kehalit@users.noreply.github.com>
2025-06-03 10:18:18 +0200

    wrapped the app with the provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import 'leaflet/dist/leaflet.css';
-
-
-
+import 'leaflet/dist/leaflet.css'
+import { AuthProvider } from "./context/AuthContext"
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <AuthProvider>
+      <App />
+    </AuthProvider> 
+  </StrictMode>
 )
