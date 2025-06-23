c749b1e9ba1758d25c043cd4f6232f82b7442073
Kehalit <88938730+kehalit@users.noreply.github.com>
2025-06-23 21:03:59 +0200

    updated app with ErrorBoundary integrated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import Router from "./routes/router";
 import Header from "./components/Header"
 import { Toaster } from 'react-hot-toast';
+import ErrorBoundary from "./components/ErrorBoundary";
 
 
 
@@ -11,10 +12,11 @@ import { Toaster } from 'react-hot-toast';
 function App() {
   return (
     <BrowserRouter>
+     <ErrorBoundary>
      <Header />
      <Toaster position="top-right" reverseOrder={false} />
       <Router />
-      
+     </ErrorBoundary>
     </BrowserRouter>
   
   );
